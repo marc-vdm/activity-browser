26b25b96eea309daa2c89f17a8401c26bff5d598
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2021-08-04 17:00:13 +0200

    cuts tree view + minor code cleanup

--- activity_browser/extensions/mlca/mLCA_table_models.py
+++ activity_browser/extensions/mlca/mLCA_table_models.py
@@ -12,7 +12,6 @@ from activity_browser.bwutils import AB_metadata
 from .mLCA_signals import mlca_signals
 from .modularsystem import ModularSystemDataManager
 
-
 class ModuleDatabaseModel(PandasModel):
     """Contain data for all modules in the modular system database."""
     HEADERS = ["Name", "out/chain/cuts", "Outputs", "Cuts", "Chain"]
@@ -62,36 +61,30 @@ class ModuleOutputsModel(PandasModel):
         return self._dataframe.iat[idx.row(), -1]
 
     def sync(self, module_name: str) -> None:
-
         for raw_module in self.manager.open_raw():
             if raw_module['name'] == module_name:
                 outputs = raw_module['outputs']
                 break
 
-        databases = set()
         output_keys = []
         for output in outputs:
             for key in output[0:-2]:
-                databases.add(key[0])
                 output_keys.append((key, output[-2], output[-1]))
 
-        databases = list(databases)
-
         data = []
-        for database in databases:
-            db = AB_metadata.get_database_metadata(database)
-            for out_key, custom_name, quantity in output_keys:
-                row = db[db['key'] == out_key]
-                data.append({
-                    "custom name": custom_name,
-                    "quantity": quantity,
-                    "unit": row['unit'].values[0],
-                    "product": row['reference product'].values[0],
-                    "name": row['name'].values[0],
-                    "location": row['location'].values[0],
-                    "database": out_key[0],
-                    "key": out_key
-                })
+        for out_key, custom_name, quantity in output_keys:
+            db = AB_metadata.get_database_metadata(out_key[0])
+            row = db[db['key'] == out_key]
+            data.append({
+                "custom name": custom_name,
+                "quantity": quantity,
+                "unit": row['unit'].values[0],
+                "product": row['reference product'].values[0],
+                "name": row['name'].values[0],
+                "location": row['location'].values[0],
+                "database": out_key[0],
+                "key": out_key
+            })
 
         self._dataframe = pd.DataFrame(data, columns=self.HEADERS)
         self.key_col = self._dataframe.columns.get_loc("key")
@@ -116,7 +109,6 @@ class ModuleChainModel(PandasModel):
         return self._dataframe.iat[idx.row(), -1]
 
     def sync(self, module_name: str) -> None:
-
         for raw_module in self.manager.open_raw():
             if raw_module['name'] == module_name:
                 chain = raw_module['chain']
@@ -138,13 +130,6 @@ class ModuleChainModel(PandasModel):
 
 class ModuleCutsItem(TreeItem):
     """ Item in ModuleCutsModel."""
-
-    #@classmethod
-    #def build_header(cls, header: str, parent: TreeItem) -> 'ModuleCutsItem':
-    #    item = cls([header, "", "", ""], parent)
-    #    parent.appendChild(item)
-    #    return item
-
     @classmethod
     def build_item(cls, cut, parent: TreeItem) -> 'ModuleCutsItem':
         item = cls(list(cut), parent)
@@ -163,8 +148,8 @@ class ModuleCutsModel(BaseTreeModel):
         self._dataframe = None
         self.key_col = 0
         self.cut_col = 0
+        self.cut_products = []
 
-        self.setup_model_data()
         self.connect_signals()
 
     def connect_signals(self):
@@ -175,48 +160,59 @@ class ModuleCutsModel(BaseTreeModel):
 
         Trigger this at the start and when a method is added/deleted.
         """
-        pass
+        for product in self.cut_products:
+            # set first branch level as name
+            prod_branch = ['' for i in range(len(self.HEADERS)-2)]  # change 2 to as many columns are hidden
+            prod_branch[0] = product.split('::')[-2]
+            prod_branch = ModuleCutsItem.build_item(prod_branch, self.root)
+            # set leaves with data
+            data = self._dataframe[self._dataframe["cut"] == product]
+            for _, row in data.iterrows():
+                ModuleCutsItem.build_item(row.to_list(), prod_branch)
 
     def sync(self, module_name: str) -> None:
+        # clear tree
         self.beginResetModel()
         self.root.clear()
         self.endResetModel()
 
+        # get data
         for raw_module in self.manager.open_raw():
             if raw_module['name'] == module_name:
                 cuts = raw_module['cuts']
                 break
 
-        databases = set()
         output_keys = []
         c = 0
+        cut_products = set()
         for cut in cuts:
             c += 1
             for key in cut[0:-2]:
-                databases.add(key[0])
-                output_keys.append((key, str(cut[-2] + '::' + str(c)), cut[-1]))
-
-        databases = list(databases)
+                _cut = str(cut[-2] + '::' + str(c))
+                output_keys.append((key, _cut, cut[-1]))
+                cut_products.add(_cut)
+        self.cut_products = list(cut_products)
 
         data = []
-        for database in databases:
-            db = AB_metadata.get_database_metadata(database)
-            for cut_key, cut, amount in output_keys:
-                row = db[db['key'] == cut_key]
-                data.append({
-                    "product": row['reference product'].values[0],
-                    "name": row['name'].values[0],
-                    "location": row['location'].values[0],
-                    "amount": amount,
-                    "unit": row['unit'].values[0],
-                    "database": cut_key[0],
-                    "key": cut_key,
-                    "cut": cut
-                })
+        for cut_key, cut, amount in output_keys:
+            db = AB_metadata.get_database_metadata(cut_key[0])
+            row = db[db['key'] == cut_key]
+            data.append({
+                "product": row['reference product'].values[0],
+                "name": row['name'].values[0],
+                "location": row['location'].values[0],
+                "amount": amount,
+                "unit": row['unit'].values[0],
+                "database": cut_key[0],
+                "key": cut_key,
+                "cut": cut
+            })
 
+        # move data to dataframe
         self._dataframe = pd.DataFrame(data, columns=self.HEADERS)
         self.key_col = self._dataframe.columns.get_loc("key")
         self.cut_col = self._dataframe.columns.get_loc("cut")
 
+        # make tree
         self.setup_model_data()
         self.updated.emit()
