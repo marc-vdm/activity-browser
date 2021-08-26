3c5f52945a2666aa5b0b46899fe74ef38d52410d
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2021-08-26 15:05:20 +0200

    Fix module changes did not reset module LCI, thus invalidating results

--- activity_browser/extensions/mlca/module.py
+++ activity_browser/extensions/mlca/module.py
@@ -69,6 +69,9 @@ class Module(object):
         self.cut_names = [c[2] for c in self.cuts]
         self.is_multi_output = len(self.outputs) > 1
 
+        if hasattr(self, "calculated_lca"):
+            del self.calculated_lca
+
     def remove_cuts_from_chain(self, chain: list, cuts: list) -> set:
         """Remove chain items if they are the parent of a cut. Otherwise this leads to unintended LCIA results."""
         for cut in cuts:
